c17be01412ad64a6ee1595c3d01da4d7ac684dcc
A-k-soni <65247356+A-k-soni@users.noreply.github.com>
2020-10-01 10:46:25 +0530

    Add files via upload

---
+++ SirPractice/src/sep24/Demo.java
@@ -0,0 +1,12 @@
+package sep24;
+
+interface Foo{
+	static void m1(){
+		System.out.println("static inteface method");
+	}
+}
+class Demo{
+	public static void main(String args[]){
+		Foo.m1();
+	}
+}
